a549d6efe3d7ccaaee21e0b573bba1ee72107212
vinaconc <vinaconc@gmail.com>
2025-10-11 01:40:05 -0300

    Update convert.py 8001 eh Flask

--- convert.py
+++ convert.py
@@ -1002,8 +1002,9 @@ def get_pdf():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
+    port = int(os.environ.get("PORT", "8001"))
     app.run(host="0.0.0.0", port=port, debug=True, threaded=True)
+
 
 
 
