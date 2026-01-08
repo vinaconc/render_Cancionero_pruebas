d748e8e2e71d37769247f25cf23e9723378e45fc
vinaconc <vinaconc@gmail.com>
2026-01-08 02:19:28 -0300

    fix5 grave

--- convert.py
+++ convert.py
@@ -224,7 +224,7 @@ def procesar_linea_con_acordes_y_indices(linea, acordes, titulo_cancion, simbolo
         if base.strip() == '_':
             if idx_acorde < len(acordes):
                 acorde = convertir_a_latex(acordes[idx_acorde])
-                resultado += f"\\[{acorde.replace('#', '\\#')}]"
+                resultado += f"\\\\[{acorde.replace('#', r'\\#')}]"
                 idx_acorde += 1
             continue
 
@@ -830,6 +830,7 @@ def get_pdf():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
     app.run(host="0.0.0.0", port=port, debug=True, threaded=True)
+
 
 
 
