eb3f43750f5b6351355a1cbc1cc867bafb519baa
vinaconc <vinaconc@gmail.com>
2026-05-17 22:36:24 -0400

    Back original

--- convert.py
+++ convert.py
@@ -215,8 +215,7 @@ def procesar_palabra_indexada(palabra, acordes, idx_acorde, titulo_cancion, indi
     else:
         base_raw = palabra_trabajo
         indice_real = base_raw.replace("_", "")
-    if es_indexada:
-        indice_real = indice_real.replace(".","").replace(";", "")
+
     partes = base_raw.split("_")
     resultado = ""
 
